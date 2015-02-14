aa5182a2d87ac70f3cf1f47773e4f6df4ad9c58c
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-14 12:11:37 -0700

    added one file

---
+++ spinnig_wheel.rb
@@ -0,0 +1,16 @@
+print "Hello"
+sleep(2)
+print "\b"
+sleep(0.2)
+print "\b"
+sleep(0.2)
+print "\b"
+sleep(0.2)
+print "\b"
+sleep(0.2)
+print "\b"
+print "How are you?"
+12.times {
+sleep(0.2)
+print "\b"
+}
