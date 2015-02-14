fd039659548ef6df4ea7bd03536dd542769ae254
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-14 12:23:12 -0700

    added 4 more lines to file

--- spinnig_wheel.rb
+++ spinnig_wheel.rb
@@ -1,2 +1,8 @@
 puts "Hello World"
 puts "Hello Hello"
+puts 'This is branch edit'
+puts "Let's mess branches"
+puts "Now master is updated"
+# We'll add couple of lines
+# This is supposed to be branch only line
+
