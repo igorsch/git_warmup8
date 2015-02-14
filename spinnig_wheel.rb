35acfd4392f9402c1293fd768ac0780324205129
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-14 12:15:36 -0700

    added 2nd line

--- spinnig_wheel.rb
+++ spinnig_wheel.rb
@@ -1,16 +1,2 @@
-print "Hello"
-sleep(2)
-print "\b"
-sleep(0.2)
-print "\b"
-sleep(0.2)
-print "\b"
-sleep(0.2)
-print "\b"
-sleep(0.2)
-print "\b"
-print "How are you?"
-12.times {
-sleep(0.2)
-print "\b"
-}
+puts "Hello World"
+puts "Hello Hello"
