4acc5fc8e2d7127a983cd2be62f8fcad3e358bbb
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-14 12:28:27 -0700

    made changes to branch file

--- spinnig_wheel.rb
+++ spinnig_wheel.rb
@@ -6,3 +6,7 @@ puts "Now master is updated"
 # We'll add couple of lines
 # This is supposed to be branch only line
 
+#Now we'll add more lines to branch
+#file
+puts "and see how"
+puts 'to merge it'
