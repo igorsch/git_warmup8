109bbe1832a3f15151011513c69d3c8e5acfb4b6
igorsch <igorsch@users.noreply.github.com>
2015-02-14 12:34:45 -0700

    Update spinnig_wheel.rb

--- spinnig_wheel.rb
+++ spinnig_wheel.rb
@@ -1,8 +1,1 @@
-puts "Hello World"
-puts "Hello Hello"
-puts 'This is branch edit'
-puts "Let's mess branches"
-puts "Now master is updated"
-# We'll add couple of lines
-# This is supposed to be branch only line
-
+#This is master file
